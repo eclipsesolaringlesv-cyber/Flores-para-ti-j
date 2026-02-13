e64c11f11e53fd4a9384b4ea3358fbe06841893c
eclipsesolaringlesv-cyber <eclipsesolaringlesv@gmail.com>
2026-02-13 05:53:24 -0600

    Add button fade-in effect after loading

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+setTimeout(() => {
+  const boton = document.querySelector(".btn-pdf");
+  if (boton) {
+    boton.style.opacity = 1;
+    boton.style.transform = "translateX(-50%) translateY(0)";
+  }
+}, 28000);
